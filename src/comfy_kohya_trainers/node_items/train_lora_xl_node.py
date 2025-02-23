f88ba6a0ec0026acf70f1f1daa90460b0937ac28
Yoonsoo Kim <laptise@live.jp>
2025-02-23 19:45:31 +0900

    use repo

--- src/comfy_kohya_trainers/node_items/train_lora_xl_node.py
+++ src/comfy_kohya_trainers/node_items/train_lora_xl_node.py
@@ -1,12 +1,43 @@
 import argparse
+import os
+import pathlib
+import subprocess
+from venv import logger
 from library.train_util import read_config_from_file
+import toml
 from ..train_lora_xl import SdxlNetworkTrainer
 from ..const import SAMPLER_CONFIG_TYPE, TRAIN_CONFIG_TYPE, OUTPUT_CONFIG_TYPE, DATASET_LOADER_TYPE, DatasetLoaderDict, TrainConfigDict
 from ..args import ClassfiedArgs
 from ..args import setup_parser_sdxl
 from accelerate import Accelerator
+import os
 
 PRECISIONS = ["fp16", "bf16"]
+
+def run_cli(command: str):
+    import subprocess
+    import sys
+
+    print(f"run cli: {command}")
+
+    # bufsize=1, text=True（universal_newlines=True）を指定して行単位で処理
+    process = subprocess.Popen(
+        command,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,  # 標準エラーも標準出力にマージ
+        text=True,
+        bufsize=1
+    )
+
+    # 標準出力をリアルタイムで読み取る
+    for line in process.stdout:
+        print(line, end="")
+
+    process.stdout.close()
+    return_code = process.wait()
+
+    return return_code
 
 class TrainLoraXlNode:
     def __init__(self):
@@ -50,18 +81,50 @@ class TrainLoraXlNode:
             network_module="networks.lora",
             dataset_config=dataset['dataset_config']
         )
+
+        file_dir = os.path.dirname(os.path.abspath(__file__))
+        node_root = file_dir.rsplit("/", 3)[0]
+        kohya_repo_dir = os.path.join(node_root, "oss", "kohya-main")
+        # check if kohya-main exists
+        if not os.path.exists(kohya_repo_dir):
+            #clone kohya-main
+            print(f"kohya-main not found at {kohya_repo_dir}, cloning...")
+            run_cli(f"git clone https://github.com/kohya-ss/sd-scripts.git {kohya_repo_dir}")
         parser = setup_parser_sdxl()
         args = read_config_from_file(args, parser)
+
+        config_path = f"{os.getcwd()}/argstest.toml"
+
+        # convert args to dictionary
+        args_dict = vars(args)
+
+        # remove unnecessary keys
+        for key in ["config_file", "output_config", "wandb_api_key"]:
+            if key in args_dict:
+                del args_dict[key]
+
+        # get default args from parser
+        default_args = vars(parser.parse_args([]))
+
+        # remove default values: cannot use args_dict.items directly because it will be changed during iteration
+        for key, value in list(args_dict.items()):
+            if key in default_args and value == default_args[key]:
+                del args_dict[key]
+
+        # convert Path to str in dictionary
+        for key, value in args_dict.items():
+            if isinstance(value, pathlib.Path):
+                args_dict[key] = str(value)
+
+        # convert to toml and output to file
+        with open(config_path, "w") as f:
+            toml.dump(args_dict, f)
+
+        logger.info(f"Saved config file / 設定ファイルを保存しました: {config_path}")
+
         # Accelerator(mixed_precision=train_config["mixed_precision"], cpu=False)
         print(f"args: {args}")
-        # print(f"dataset: {dataset}")
-        # print(f"train_config: {train_config}")
-        # print(f"sampler_config: {sampler_config}")
-        # print(f"output_config: {output_config}")
-        trainer = SdxlNetworkTrainer()
-        trainer.train(args)
-        # set_auth_config(r_token=huggingface_token, w_token=huggingface_token)
-        # dset = load_dataset_for_dpack(dataset_repo_id)
-        # print(f"loaded dataset: {dataset_repo_id}")
-        # return (dset_info, )
+        cmd = f"accelerate launch --mixed_precision {train_config['mixed_precision']} {kohya_repo_dir}/train_network.py --config_file {config_path} "
+        run_cli(cmd)
+
         return ("a", )
